d44909409ab87b2e375c3097b4a9d29395812b2f
Cemileb <cemilebalkas@gmail.com>
2023-12-19 16:41:21 +0000

    added comments

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,8 @@ docBody.addEventListener("keydown", function(event){
         let backgroundImg = document.createElement('img');
         
         backgroundImg.setAttribute("src", "https://c4.wallpaperflare.com/wallpaper/586/603/742/minimalism-4k-for-mac-desktop-wallpaper-preview.jpg");
-
+        //the code below is what I normally add when adding a background image but this just seems to be adding a pic below the 
+        // boxes of html. I couldn't figure out why it doesnt treat it as background image 
         docBody.setAttribute("style", "background-image: backgroundImg");
         docBody.setAttribute("style", 'background-size: cover;');
         docBody.setAttribute("style", 'background-position: center;');
